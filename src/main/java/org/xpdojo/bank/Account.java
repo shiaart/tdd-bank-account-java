f604d8e2b2ce0e0e59f566dd398eb73f25aaad05
shiaart <shiabutdinov@gmail.com>
2020-07-16 17:01:17 +0300

    initial commit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,17 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance;
+
+    public void Account(){
+        balance = 0;
+    }
+    public int balance(){
+        return balance;
+    }
+
+    public int deposit(int ammount){
+        balance +=ammount;
+        return balance;
+    }
 }
